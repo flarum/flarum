8ee8a0a9b9ac5ad9c9530eec3e9073ee21495c39
Franz Liedke <franz@develophp.org>
2015-03-26 21:04:00 +0100

    Configure filesystem disk for avatars.

--- config/filesystems.php
+++ config/filesystems.php
@@ -43,26 +43,9 @@ return [
 
 	'disks' => [
 
-		'local' => [
+		'avatars' => [
 			'driver' => 'local',
-			'root'   => storage_path().'/app',
-		],
-
-		's3' => [
-			'driver' => 's3',
-			'key'    => 'your-key',
-			'secret' => 'your-secret',
-			'region' => 'your-region',
-			'bucket' => 'your-bucket',
-		],
-
-		'rackspace' => [
-			'driver'    => 'rackspace',
-			'username'  => 'your-username',
-			'key'       => 'your-key',
-			'container' => 'your-container',
-			'endpoint'  => 'https://identity.api.rackspacecloud.com/v2.0/',
-			'region'    => 'IAD',
+			'root'   => public_path('avatars')
 		],
 
 	],
