6312d94533aecd890528de7d46096b4aa1ca617b
Toby Zerner <toby.zerner@gmail.com>
2015-08-02 17:42:13 +0930

    Update admin front controller

--- admin.php
+++ admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use Flarum\Core;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Zend\Diactoros\Server;
@@ -9,14 +10,20 @@ use Zend\Stratigility\MiddlewarePipe;
 $app = require __DIR__.'/system/bootstrap.php';
 
 // Set up everything we need for the frontend
-$app->instance('type', 'admin');
 $app->register('Flarum\Admin\AdminServiceProvider');
-$app->register('Flarum\Support\Extensions\ExtensionsServiceProvider');
 
+// Build a middleware pipeline for Flarum
 $admin = new MiddlewarePipe();
+$admin->pipe($app->make('Flarum\Api\Middleware\ReadJsonParameters'));
 $admin->pipe($app->make('Flarum\Admin\Middleware\LoginWithCookieAndCheckAdmin'));
 $admin->pipe('/admin', $app->make('Flarum\Http\RouterMiddleware', ['routes' => $app->make('flarum.admin.routes')]));
-$admin->pipe(new \Franzl\Middleware\Whoops\Middleware());
+
+// Handle errors
+if (Core::inDebugMode()) {
+    $admin->pipe(new \Franzl\Middleware\Whoops\Middleware());
+} else {
+    $admin->pipe(new \Flarum\Forum\Middleware\HandleErrors(base_path('error')));
+}
 
 $server = Server::createServer(
 	$admin,
