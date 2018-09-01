54d9228ab835bd8686fb8198e7f092fe18bc5f72
Franz Liedke <franz@develophp.org>
2018-09-01 15:44:53 +0200

    Add a root-level extend.php for per-forum customizations

---
+++ extend.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Flarum\Extend;
+
+return [
+    // Register extenders here to customize your forum!
+];
