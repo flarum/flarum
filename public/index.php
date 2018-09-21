655aa9cea91dab571b67d917f37cc95ce3615285
Franz Liedke <franz@develophp.org>
2018-09-21 23:35:34 +0200

    Booting is now done by the Server classes
    Refs flarum/core#1421.

--- public/index.php
+++ public/index.php
@@ -11,15 +11,11 @@
 
 require '../vendor/autoload.php';
 
-$site = Flarum\Foundation\Site::fromPaths([
-    'base' => __DIR__.'/..',
-    'public' => __DIR__,
-]);
-
-$app = $site->bootApp();
-
 $server = new Flarum\Http\Server(
-    $app->getRequestHandler()
+    Flarum\Foundation\Site::fromPaths([
+        'base' => __DIR__.'/..',
+        'public' => __DIR__,
+    ])
 );
 
 $server->listen();
