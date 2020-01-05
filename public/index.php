abe7a51cf57f04ca4459541753914f1e60c363c0
Daniël Klabbers <luceos@users.noreply.github.com>
2020-01-05 22:31:04 +0100

    update index.php with new license

--- public/index.php
+++ public/index.php
@@ -1,12 +1,9 @@
 <?php
-
 /*
  * This file is part of Flarum.
  *
- * (c) Toby Zerner <toby.zerner@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
  */
 
 require '../vendor/autoload.php';
