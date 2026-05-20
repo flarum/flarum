229402d21e7674b721d2bcaecf9e58491871ad69
0xAndreSec <arzenqwee@gmail.com>
2026-05-20 14:15:27 +0300

    Update extend.php

--- extend.php
+++ extend.php
@@ -1,14 +1,8 @@
 <?php
 
-/*
- * This file is part of Flarum.
- *
- * For detailed copyright and license information, please view the
- * LICENSE file that was distributed with this source code.
- */
+declare(strict_types=1);
 
 use Flarum\Extend;
 
 return [
-    // Register extenders here to customize your forum!
 ];
